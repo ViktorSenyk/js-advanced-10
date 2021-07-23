de5b4b6841bac72196268e40dd6dfb4c90efe2a8
ViktorSenyk <victor.senyk.vs@gmail.com>
2021-07-23 20:06:32 +0300

    Some...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorTopComponent } from './editor-top/editor-top.component';
+import { EditorMiddleComponent } from './editor-middle/editor-middle.component';
+import { EditorBottomComponent } from './editor-bottom/editor-bottom.component';
+import {FormsModule} from "@angular/forms";
+import { ColorBlockComponent } from './color-block/color-block.component';
+import { BackgroundColorBlockComponent } from './background-color-block/background-color-block.component';
+import { EditorAddComponent } from './editor-add/editor-add.component';
+import { NoSanitizePipe } from './no-sanitize.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorTopComponent,
+    EditorMiddleComponent,
+    EditorBottomComponent,
+    ColorBlockComponent,
+    BackgroundColorBlockComponent,
+    EditorAddComponent,
+    NoSanitizePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
